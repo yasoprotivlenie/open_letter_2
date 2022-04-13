577e478118b14410a07360e02d06a44a9cefebad
Maxim Matyunin <matyunya@gmail.com>
2022-04-13 22:23:41 +0900

    try publish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,46 @@
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": theme("colors.gray[800]"),
+            "--tw-prose-headings": theme("colors.gray[900]"),
+            "--tw-prose-lead": theme("colors.gray[700]"),
+            "--tw-prose-links": theme("colors.blue[600]"),
+            "--tw-prose-bold": theme("colors.gray[900]"),
+            "--tw-prose-counters": theme("colors.gray[600]"),
+            "--tw-prose-bullets": theme("colors.gray[400]"),
+            "--tw-prose-hr": theme("colors.gray[300]"),
+            "--tw-prose-quotes": theme("colors.gray[900]"),
+            "--tw-prose-quote-borders": theme("colors.gray[300]"),
+            "--tw-prose-captions": theme("colors.gray[700]"),
+            "--tw-prose-code": theme("colors.gray[900]"),
+            "--tw-prose-pre-code": theme("colors.gray[100]"),
+            "--tw-prose-pre-bg": theme("colors.gray[900]"),
+            "--tw-prose-th-borders": theme("colors.gray[300]"),
+            "--tw-prose-td-borders": theme("colors.gray[200]"),
+            "--tw-prose-invert-body": theme("colors.gray[200]"),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme("colors.gray[300]"),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme("colors.gray[400]"),
+            "--tw-prose-invert-bullets": theme("colors.gray[600]"),
+            "--tw-prose-invert-hr": theme("colors.gray[700]"),
+            "--tw-prose-invert-quotes": theme("colors.gray[100]"),
+            "--tw-prose-invert-quote-borders": theme("colors.gray[700]"),
+            "--tw-prose-invert-captions": theme("colors.gray[400]"),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme("colors.gray[300]"),
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": theme("colors.gray[600]"),
+            "--tw-prose-invert-td-borders": theme("colors.gray[700]"),
+          },
+        },
+      }),
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
